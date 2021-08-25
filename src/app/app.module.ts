703a0889bb1162491cc5b0029deea2a3ddce70fd
Emma Åkerberg <emma.akerberg@se.experis.com>
2021-08-25 09:44:56 +0200

    feat: created start page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StartPageComponent } from './start-page/start-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartPageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
